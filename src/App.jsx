d3de52b9fda3de1e0e6dcaaae284d9d9857bc00c
lorena252 <lorenaavalos252@hotmail.com>
2024-02-13 10:03:38 -0300

    i modify the fetch and create CardPokemon component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-
+import ContainCardsPokemon from "./components/ContainCardsPokemon";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header/>
         <Routes>
-          <Route></Route>
+        <Route path="/" element={<ContainCardsPokemon />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/ContainCardsPokemon.jsx
+++ src/components/ContainCardsPokemon.jsx
@@ -1,7 +1,47 @@
-export default function ContainCardsPokemon() {
-return(
-<>
+import { useEffect, useState } from "react";
+import { Flex, Box, Wrap ,Center} from "@chakra-ui/react";
+import CardPokemon from "./CardPokemon";
 
-</>
-)
+export default function ContainCardsPokemon() {
+  const [pokemons, setPokemons] = useState([]);
+
+
+  useEffect(() => {
+    let dataPoke = [];
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => response.json())
+      .then((data) => {
+        dataPoke = data.results.map((poke) => fetch(poke.url));
+        Promise.all(dataPoke).then((pokemon) => {
+          Promise.all(pokemon.map((poke) => poke.json())).then((data) => {
+            setPokemons(data);
+            console.log(data);
+          });
+        });
+      });
+  }, []);
+
+  return (
+    <Box>
+
+      <Flex>
+        <Wrap>
+         
+          {pokemons.map((pokemon) => {
+            return (
+              <CardPokemon
+                id={pokemon.id}
+                key={pokemon.name}
+                name={pokemon.name}
+                image={pokemon.sprites.front_default}
+                
+              />
+            );
+          })}
+          
+        </Wrap>
+      </Flex>
+
+    </Box>
+  );
 }
